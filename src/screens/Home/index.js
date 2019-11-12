fbce4b78ea32153feca36082c092b76840da4307
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-13 00:57:20 +0100

    partial layout

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { View, Text } from 'react-native';
+import { FlatList } from 'react-native';
+
+import {
+  Container,
+  List,
+  Product,
+  Header,
+  ProductImage,
+  Title,
+  Price,
+  ProductButton,
+  ButtonIconContainer,
+  ProductButtonAdded,
+  ProductButtonText,
+} from './styles';
 
 const Home = () => {
+  const products = [
+    {
+      id: 1,
+      title: 'Performance shoes',
+      price: 1179.9,
+      image:
+        'https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg',
+    },
+    {
+      id: 2,
+      title: 'Nice shoes for sports',
+      price: 1539.9,
+      image:
+        'https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg',
+    },
+    {
+      id: 3,
+      title: 'Addidas Shoes',
+      price: 1599.99,
+      image:
+        'https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/9920dfd3593b4e068696a9840160d43f_9366/SoleCourt_Clay_Shoes_Svart_G26293_01_standard.jpg',
+    },
+    {
+      id: 5,
+      title: 'GEL-NIMBUS™ 22',
+      price: 1900,
+      image:
+        'https://asics.scene7.com/is/image/asics/1011A680_023_SR_RT?$zoom$',
+    },
+    {
+      id: 6,
+      title: 'GEL-NIMBUS™ 21 LS',
+      price: 2000,
+      image:
+        'https://asics.scene7.com/is/image/asics/1012A540_001_SR_RT?$sfcc-product$',
+    },
+    {
+      id: 4,
+      title: 'GEL-NIMBUS™ 21 WINTERIZED',
+      price: 1900,
+      image:
+        'https://asics.scene7.com/is/image/asics/1011A633_020_SR_RT?$zoom$',
+    },
+  ];
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <List
+        data={products}
+        horizontal
+        keyExtractor={product => String(product.id)}
+        renderItem={({ item }) => (
+          <Product>
+            <Header>
+              <Title>{item.title}</Title>
+              <ProductImage
+                source={{
+                  uri: String(item.image),
+                }}
+              />
+              <Price>{item.price}</Price>
+            </Header>
+            <ProductButton onPress={() => {}}>
+              <ButtonIconContainer>
+                <Icon name="add_shopping_card" size={25} />
+                <ProductButtonAdded>1</ProductButtonAdded>
+              </ButtonIconContainer>
+              <ProductButtonText>ADD TO CART</ProductButtonText>
+            </ProductButton>
+          </Product>
+        )}
+      />
+    </Container>
   );
 };
 
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,1 +1,71 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+  background: #fff;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const Product = styled.View`
+  justify-content: flex-start;
+  align-content: center;
+  width: 260px;
+  margin: 30px 10px;
+  border-radius: 4px;
+  border: 1px solid #eee;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+`;
+
+export const ProductImage = styled.Image`
+  align-content: center;
+  width: 250px;
+  height: 150;
+  border-radius: 4px;
+  background: #eee;
+`;
+
+export const Title = styled.Text`
+  font-size: 14px;
+  color: #333;
+  font-weight: bold;
+  margin-top: 4px;
+  text-align: center;
+`;
+
+export const Price = styled.Text`
+  font-size: 21px;
+  font-weight: bold;
+  margin: 5px 0 20px;
+`;
+
+export const ProductButton = styled(RectButton)`
+  justify-content: space-between;
+  align-content: space-between;
+  margin: 10px;
+  background: #7159c1;
+  color: #fff;
+  border: 0;
+  border-radius: 4px;
+`;
+
+export const ButtonIconContainer = styled.View`
+  flex: 1;
+`;
+
+export const ProductButtonAdded = styled.Text`
+  text-align: center;
+`;
+
+export const ProductButtonText = styled.Text`
+  text-align: center;
+`;
